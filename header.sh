dbf4f466ca5817e2fdbd8f377c7297f764d00b03
Ilenia <ile.carboni@gmail.com>
2022-08-11 18:29:54 +0200

    Update header.sh

--- header.sh
+++ header.sh
@@ -3,8 +3,10 @@ mkdir -p ~/.vim
 #entra en el directorio plugin que hemos creado
 cd ~/.vim
 #clona el plugin necesario para añadir el header usando vim desde el repositorio de github
-git clone https://github.com/pbondoer/vim-42header.git .
+git clone https://github.com/pbondoer/vim-42header.git clone
 #vuelve a la raíz(root)
+mv ~/.vim/vim-42header/after ~/.vim
+rm -fR ~/.vim/vim-42header
 cd ~
 #crea un archivo ".zshrc"
 #es necesario modificar "user" con tu propio nombre de usuario (el que usas para el login)
